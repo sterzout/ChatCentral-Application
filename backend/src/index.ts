fc1f459dd13e86d06c31686cc7cea7b66974d3d6
Salim Terzout <sterzout@uwo.ca>
2025-02-11 00:28:17 -0500

    Update repository with latest local changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
 dotenv.config();
 
 
 const app = express()
+
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
